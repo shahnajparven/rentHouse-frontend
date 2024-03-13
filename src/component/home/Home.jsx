076145554c57f1583b30ad6a719945c109c4848e
shahnaj parven <shahnajritu21@gmail.com>
2024-03-13 14:02:50 +0600

    reward add

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
 import { Banner } from "../banner/Banner";
 import Product from "./Product";
+import Reward from "../reward/Reward";
 
 const Home = () => {
   return (
     <Box>
       <Banner />
       <Product />
+      <Reward />
     </Box>
   );
 };
---
+++ src/component/reward/Reward.jsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import Card from 'react-bootstrap/esm/Card';
+import CountUp from 'react-countup';
+import './Reward.css';
+import { Box, Divider } from '@mui/material';
+
+const Reward = () => {
+    const [loading, setLoading] = React.useState(false);
+
+    const onStart = () => {
+        setLoading(true);
+    };
+
+    const onEnd = () => {
+        setLoading(false);
+    };
+
+    const containerProps = {
+        'aria-busy': loading,
+    };
+
+
+    return (
+        <>
+        <Box px={{lg:10,md:2,sm:2,xs:2}} py={5}>
+            <Row>
+                <Col lg="7" md="6" xs="12">
+                    <Card className="reward-card1">
+                        <Card.Body>
+                            <Card.Title className="reward-title">
+                                    <h2><span className="count"><CountUp
+                                        end={70000}
+                                        duration="3"
+                                        onStart={onStart}
+                                        onEnd={onEnd}
+                                        containerProps={containerProps}
+                                    /></span> <span>+</span></h2>
+                                    <p>Happy Clients we have served</p>
+
+                             
+                                    {/* <img src={grid1} /> */}
+                             
+
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="5" md="6" xs="12">
+                    <Card className="reward-card2">
+                        <Card.Body>
+                            <Card.Title className="reward-title">
+                                <h2><span className="count"><CountUp
+                                    end={31000}
+                                    duration="3"
+                                    onStart={onStart}
+                                    onEnd={onEnd}
+                                    containerProps={containerProps}
+                                /></span> <span>+</span></h2>
+                                <p>Online members and fans</p>
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg="5" md="6" xs="12">
+                    <Card className="reward-card3">
+                        <Card.Body>
+                            <Card.Title className="reward-title">
+                                <h2><span className="count"><CountUp
+                                    end={6000}
+                                    duration="3"
+                                    onStart={onStart}
+                                    onEnd={onEnd}
+                                    containerProps={containerProps}
+                                /></span> <span>+</span></h2>
+                                <p>Completed Services</p>
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="4" md="6" xs="12">
+
+                    <Card className="reward-card4">
+                        <Card.Body>
+                            <Card.Title className="reward-title">
+                                <h2><span className="count"><CountUp
+                                    end={100}
+                                    duration="3"
+                                    onStart={onStart}
+                                    onEnd={onEnd}
+                                    containerProps={containerProps}
+                                /></span> <span>+</span></h2>
+                                <p>Search Request on last month</p>
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg="3" md="12" xs="12">
+                    <Card className="reward-card5">
+                        <Card.Body>
+                            <Card.Title className="reward-title">
+                                <h2 className="count"><CountUp
+                                    end={21}
+                                    duration="3"
+                                    onStart={onStart}
+                                    onEnd={onEnd}
+                                    containerProps={containerProps}
+                                /></h2>
+                                <p>Area we serve</p>
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+           
+        </Box>
+      
+         <Divider/>
+        </>
+
+
+    );
+
+};
+export default Reward;
